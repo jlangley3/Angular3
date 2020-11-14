77518963beed5a2aed6c9cdb45713f1af2765e41
jlangley3 <jlangleyiii@aol.com>
2020-11-14 13:47:44 -0500

    working on components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
