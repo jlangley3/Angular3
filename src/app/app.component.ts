779786869e233dc2ca56780aae46073eea0349ee
jlangley3 <jlangleyiii@aol.com>
2020-11-14 13:45:53 -0500

    working on functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My App';
+  name:string = 'My App';
+
+  constructor() {
+    this.changeName("Jay");
+  }
+
+  changeName(name:string):void {
+    this.name = name;
+  }
+
 }
