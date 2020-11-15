e46d5d7be28ddfa3f6773216d401007cd49828cd
jlangley3 <jlangleyiii@aol.com>
2020-11-15 18:40:49 -0500

    afixing title

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { Todo } from "src/app/models/Todo"
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-
+  @Input() todo: Todo;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
